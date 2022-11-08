bc2ce661df11126f77edf4b9bdad891547214281
HappyLightning <81601280+HappyLightning@users.noreply.github.com>
2022-11-08 15:16:10 -0300

    Routes e ViewSets

--- API Zoologico/setup/urls.py
+++ API Zoologico/setup/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from zoologico.views import animais
+from django.urls import path, include
+from zoologico.views import AnimaisViewSet, ZoologicosViewSet
+from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register('animais', AnimaisViewSet, basename='Animais')
+router.register('zoologicos', ZoologicosViewSet, basename='Zoologicos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('animais/', animais)
+    path('', include(router.urls))
 ]
--- API Zoologico/zoologico/views.py
+++ API Zoologico/zoologico/views.py
@@ -1,6 +1,16 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from zoologico.models import Animal, Zoologico
+from zoologico.serializer import AnimalSerializer, ZoologicoSerializer
 
 
-def animais(request):
-	if request.method == 'GET': 
-		pass
+class AnimaisViewSet(viewsets.ModelViewSet):
+	""" Todos os animais """
+	queryset = Animal.objects.all()
+	serializer_class = AnimalSerializer
+
+
+class ZoologicosViewSet(viewsets.ModelViewSet):
+	""" Todos os zoológicos """
+	queryset = Zoologico.objects.all()
+	serializer_class = ZoologicoSerializer
+	
